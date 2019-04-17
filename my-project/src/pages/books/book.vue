928722b122016151c946f94f122bca2ed5c72085
nhclike <2471814041@qq.com>
2019-04-17 17:46:12 +0800

    增加图书主页面

---
+++ my-project/src/pages/books/book.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg:'book',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-project/src/pages/me/me.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg :'个人中心页面',
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
